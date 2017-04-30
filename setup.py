6914a716adeea27567f66a7500e2561e08c63140
Aleksandar Krnjaic <aleks.krnjaic@gmail.com>
2017-05-01 00:32:10 +1000

    converting to pip package

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+# http://python-packaging.readthedocs.io/en/latest/minimal.html
+
+from setuptools import setup
+
+setup(name='intra',
+      version='0.1',
+      description='python introspection tool',
+      url='https://bitbucket.org/aleks_krnjaic/intra',
+      author='Aleks Krnjaic',
+      author_email='aleks.krnjaic@gmail.com',
+      license='MIT',
+      packages=['intra'],
+      zip_safe=False)
