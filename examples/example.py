e6080bb2ed9d19b9980976003fa74add4fd85093
Aleksandar Krnjaic <aleks.krnjaic@gmail.com>
2017-05-01 01:54:48 +1000

    modified example

--- examples/example.py
+++ examples/example.py
@@ -33,50 +33,52 @@ class SomeObject:
 
 
 
-ui = IntraUI()
+if __name__ == "__main__":
 
-# have some objects you want to analyze
-my_obj = SomeObject()
-another_obj = AnotherObject()
+    ui = IntraUI()
 
-# perform the introspection. This gives you an IntrospectionResult which you can then view via command line or UI.
-inspection1 = introspect(my_obj)
-inspection2 = introspect(another_obj)
+    # have some objects you want to analyze
+    my_obj = SomeObject()
+    another_obj = AnotherObject()
 
-# lets view it using the UI. You can add multiple inspections to the same viewer.
-viewer = ui.createViewer("My Viewer Title")
-viewer.add(inspection1)
-viewer.add(inspection2)
+    # perform the introspection. This gives you an IntrospectionResult which you can then view via command line or UI.
+    inspection1 = introspect(my_obj)
+    inspection2 = introspect(another_obj)
 
-# lets modify the objects and reanalyze them
-my_obj.add_nested_object(another_obj)
+    # lets view it using the UI. You can add multiple inspections to the same viewer.
+    viewer = ui.createViewer("My Viewer Title")
+    viewer.add(inspection1)
+    viewer.add(inspection2)
 
-# if you set the recurse flag to true, all sub-objects within the object being introspected will be traversed as well, recursively.
-#     Arguments:
-#    recurse -- whether to recurse through classes contained within the object. Good to use if there are no circular references within the object being analyzed.
-reinspected = introspect(my_obj, recurse=True)
+    # lets modify the objects and reanalyze them
+    my_obj.add_nested_object(another_obj)
 
-# View it using the UI. We'll create a new viewer here.
-viewer2 = ui.createViewer("After adding a nested object")
-viewer2.add(reinspected)
+    # if you set the recurse flag to true, all sub-objects within the object being introspected will be traversed as well, recursively.
+    #     Arguments:
+    #    recurse -- whether to recurse through classes contained within the object. Good to use if there are no circular references within the object being analyzed.
+    reinspected = introspect(my_obj, recurse=True)
 
-# you can print the introspection if you're using a shell. Pass the inspection to iprint.
-#    Arguments:
-#    pretty_print -- Whether to wrap the output in a border for easier debugging
-#    pretty_print_title -- What the wrapped output title will be
+    # View it using the UI. We'll create a new viewer here.
+    viewer2 = ui.createViewer("After adding a nested object")
+    viewer2.add(reinspected)
 
-print("\n\nTwo separate objects:\n\n")
-iprint(inspection1)
-iprint(inspection2)
+    # you can print the introspection if you're using a shell. Pass the inspection to iprint.
+    #    Arguments:
+    #    pretty_print -- Whether to wrap the output in a border for easier debugging
+    #    pretty_print_title -- What the wrapped output title will be
 
-print("\n\nNested object:\n\n")
-iprint(reinspected)
+    print("\n\nTwo separate objects:\n\n")
+    iprint(inspection1)
+    iprint(inspection2)
 
-# To see what data the introspection actually stores, we can just introspect our introspections
-# inception = introspect(my_obj, recurse=True)
-# viewer2.add(inception)
+    print("\n\nNested object:\n\n")
+    iprint(reinspected)
 
-print('you can run your own blocking code while introspections are occurring on a separate thread.')
-while True:
-    time.sleep(5)
-    pass
+    # To see what data the introspection actually stores, we can just introspect our introspections
+    inception = introspect(my_obj, recurse=True)
+    viewer2.add(inception)
+
+    print('you can run your own blocking code while introspections are occurring on a separate thread.')
+    while True:
+        time.sleep(5)
+        pass
